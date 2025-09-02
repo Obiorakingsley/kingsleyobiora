4f3138a5fa294997686df6e05e55fc7cb700831e
Obiora Kingsley <kingsleyobiora527@gmail.com>
2025-09-02 11:25:11 +0100

    Add transitions to NavLinks in Sidebar.tsx and custom html body tags to Layout.tsx

--- src/Component/Layout/Layout.tsx
+++ src/Component/Layout/Layout.tsx
@@ -1,13 +1,26 @@
-import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
 
-type Props = {};
+//type Props = {};
 
-const Layout = (props: Props) => {
+const Layout = () => {
   return (
-    <>
+    <div className="">
       <Sidebar />
-    </>
+      <div className="pl-20 min-h-screen">
+        <span className="text-primary absolute top-12 left-40 font-Aurore">
+          &lt;body&gt;
+        </span>
+        <Outlet />
+        <span className="text-primary absolute bottom-12 left-40 font-Aurore">
+          &lt;/body&gt;
+        </span>
+        <br />
+        <span className="text-primary absolute bottom-5 left-32 font-Aurore">
+          &lt;/html&gt;
+        </span>
+      </div>
+    </div>
   );
 };
 
--- src/Component/Sidebar/Sidebar.tsx
+++ src/Component/Sidebar/Sidebar.tsx
@@ -2,27 +2,72 @@ import { Link, NavLink } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
-//type Props = {};
+type isActive = {
+  isActive: boolean;
+};
 
 const Sidebar = () => {
   return (
-    <div className="min-h-screen w-20 px-8 py-4 absolute top-0 bg-slate-800 items-center flex gap-32 flex-col">
+    <div className="min-h-screen w-20 px-8 py-4 absolute top-0 bg-neutral-950 items-center flex gap-28 flex-col">
       <Link to={"/"}>
-        <img className="h-auto w-8" src={Logo} alt="logo" width={30} />
-        <p className="text-white text-lg">Kingsley</p>
+        <img className="size-8 mx-auto" src={Logo} alt="logo" width={30} />
+        <p className="text-white text-sm">Kingsley</p>
       </Link>
       <nav className="flex-1 flex gap-8 flex-col">
-        <NavLink to="/">
+        <NavLink
+          className={`home ${({ isActive }: isActive) => {
+            isActive && "text-primary";
+          }}`}
+          to="/"
+        >
           <FontAwesomeIcon className="size-6" icon={faHome} />
         </NavLink>
-        <NavLink to="about">
+        <NavLink
+          className={`about-link ${({ isActive }: isActive) => {
+            isActive && "text-primary";
+          }}`}
+          to="about"
+        >
           <FontAwesomeIcon className="size-6" icon={faUser} />
         </NavLink>
-        <NavLink to="contact">
+        <NavLink
+          className={`contact-link ${({ isActive }: isActive) => {
+            isActive && "text-primary";
+          }}`}
+          to="contact"
+        >
           <FontAwesomeIcon className="size-6" icon={faEnvelope} />
         </NavLink>
       </nav>
+      <ul className="list-none flex flex-col gap-6 ">
+        <Link to="">
+          <li>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </li>
+        </Link>
+        <Link to="">
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+          </li>
+        </Link>
+        <Link to="">
+          <li>
+            <FontAwesomeIcon icon={faTwitter} />
+          </li>
+        </Link>
+        <Link to="">
+          <li>
+            <FontAwesomeIcon icon={faInstagram} />
+          </li>
+        </Link>
+      </ul>
     </div>
   );
 };
