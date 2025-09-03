bb8b290cf68a0f6c9e6a12e6415d19e9e44aa7cb
Obiora Kingsley <kingsleyobiora527@gmail.com>
2025-09-03 04:30:43 +0100

    Add home component with animated hero section

--- src/Component/Home/Home.tsx
+++ src/Component/Home/Home.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import icon from "../../assets/images/logo.png";
 
-type Props = {};
-
-const Home = (props: Props) => {
-  return <div>Home</div>;
+const Home = () => {
+  return (
+    <div className="home-page">
+      <div className="container hero-text top-1/4 w-full px-12">
+        <h1 className="text-white text-5xl sm:text-6xl">
+          Hi, <br />
+          <span className="whitespace-nowrap flex">
+            I'm
+            <img className="ml-4 size-14 sm:size-14" src={icon} alt="logo" />
+            ingsley
+          </span>
+          web developer
+        </h1>
+        <h2 className="font-bold text-stone-500">
+          Frontend Developer / React Specialist
+        </h2>
+        <div className="flex items-center mt-8 gap-4 text-2xl text-yellow-400">
+          <Link to="contact">Contact Me</Link>
+          <Link to="Upwork">Hire Me</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Component/Layout/Layout.tsx
+++ src/Component/Layout/Layout.tsx
@@ -7,16 +7,16 @@ const Layout = () => {
   return (
     <div className="">
       <Sidebar />
-      <div className="sm:pl-20 min-h-screen">
-        <span className="text-primary absolute sm:top-12 top-20 left-5 sm:left-32 font-Aurore">
+      <div className="sm:pl-20 p-2 min-h-screen">
+        <span className="text-primary absolute sm:top-12 top-20 left-8 sm:left-28 font-Aurore">
           &lt;body&gt;
         </span>
         <Outlet />
-        <span className="text-primary absolute bottom-12 left-5 sm:left-32 font-Aurore">
+        <span className="text-primary absolute bottom-12 left-8 sm:left-28 font-Aurore">
           &lt;/body&gt;
         </span>
         <br />
-        <span className="text-primary absolute bottom-5 sm:left-24 font-Aurore">
+        <span className="text-primary absolute bottom-5 sm:left-20 font-Aurore">
           &lt;/html&gt;
         </span>
       </div>
